764d3d42903e7ecb3212ec307df9377a31136b76
Leamsi Escribano <leamsi.esc@gmail.com>
2020-07-20 14:57:51 -0400

    Fixing git bug

---
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -0,0 +1,11 @@
+class TrainersController < ApplicationController
+  def index
+    trainers = Trainer.all
+    render json: trainers
+  end
+
+  def show
+    trainer = Trainer.find(params[:id])
+    render json: trainer.as_json(only: :name, include: { pokemons: { except: [:created_at, :updated_at] } })
+  end
+end
