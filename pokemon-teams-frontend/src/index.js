7406216a32d15e630310976c9bad27776a09af93
shiro-han <shan99business@gmail.com>
2020-07-20 15:11:06 -0400

    backend done!

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,34 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+const fetchTrainers = () => {
+    return fetch(`${TRAINERS_URL}`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+const fetchPokemon = () => {
+    return fetch(`${POKEMONS_URL}`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+const addPokemon = (pokeObject) => {
+    return fetch(`${POKEMONS_URL}`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify(pokeObject)
+    }).then(response => response.json())
+    .then(data => console.log(data))
+}
+
+const releasePokemon = (id) => {
+    return fetch(`${POKEMONS_URL}/${id}`, {
+        method: 'DELETE'
+    }).then(response => response.json())
+    .then(data => console.log(data))
+}
