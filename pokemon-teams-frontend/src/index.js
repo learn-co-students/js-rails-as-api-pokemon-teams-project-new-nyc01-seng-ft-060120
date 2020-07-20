e9641afc56a39801f7688aea7b75668a2bb47a0d
mstaso <mstaso1@gmail.com>
2020-07-20 16:42:12 -0400

    added if statement in render Pokemon

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,22 +1,24 @@
 const BASE_URL = "http://localhost:3000/api/v1"
+let trainers 
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 const fetchTrainers = () => {
   fetch(TRAINERS_URL)
     .then((resp) => resp.json())
-    .then((trainers) => renderTrainers(trainers))
+    .then((data) => renderTrainers(data))
     .catch(error => console.log(error))
 }
 
 const fetchPokemons = () => {
   fetch(POKEMONS_URL)
     .then((resp) => resp.json())
-    .then((pokemons) => renderPokemons(pokemons))
+    .then((pokemons) => renderPokemon(pokemons))
     .catch(error => console.log(error))
 };
 
-const renderTrainers = (trainers) => {
+const renderTrainers = (data) => {
+  trainers = data
   trainers.forEach(trainer => renderTrainer(trainer))
 }
 
@@ -33,25 +35,29 @@ const renderTrainer = (trainer) => {
   container.appendChild(addPokeBtn)
 }
 
-const renderPokemons = (pokemons) => {
-  pokemons.forEach(pokemon => renderPokemon(pokemon))
-}
 
-const renderPokemon = (pokemon) => {
-  //let trainerDiv = document.querySelector(`[data-id='${pokemon.trainer_id}']`);
-  const pokemonsUl = document.createElement("ul");
 
-  console.log(pokemon.trainer_id)
-  const pokeLi = document.createElement('li')
-  const removePokeBtn = document.createElement("button");
-  pokeLi.innerHTML = pokemon.name
-  
-  removePokeBtn.innerHTML = "Remove Pokemon";
-  removePokeBtn.classList.add('release')
-  removePokeBtn.dataset.pokemonid = pokemon.id
-  pokemonsUl.appendChild(pokeLi)
-  pokemonsUl.appendChild(removePokeBtn)
-  //trainerDiv.appendChild(pokemonsUl)
+const renderPokemon = (pokemons) => {
+  Array.from(pokemons).filter(pokemon => 
+    {if (pokemon.trainer_id = trainers.id){
+    pokemons.slice(0, 5).forEach(pokemon => {
+      let trainerDiv = document.querySelector(`[data-id='${pokemon.trainer_id}']`);
+      const pokemonsUl = document.createElement("ul");
+    
+      console.log(pokemon.trainer_id)
+      const pokeLi = document.createElement('li')
+      const removePokeBtn = document.createElement("button");
+      pokeLi.innerHTML = pokemon.nickname
+      
+      removePokeBtn.innerHTML = "Remove Pokemon";
+      removePokeBtn.classList.add('release')
+      removePokeBtn.dataset.pokemonid = pokemon.id
+      pokemonsUl.appendChild(pokeLi)
+      pokemonsUl.appendChild(removePokeBtn)
+      trainerDiv.appendChild(pokemonsUl)
+    })}}}
+    
+    
 
 };
 
