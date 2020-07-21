9a0b15d0392f694dd1563ceee31b24cf4cab1dfd
mayalexandera <maya.blevins@gmail.com>
2020-07-20 20:08:30 -0400

    worked within controllers to manage promise return value

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,69 +1,58 @@
 const BASE_URL = "http://localhost:3000/api/v1"
-let trainers 
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 const fetchTrainers = () => {
   fetch(TRAINERS_URL)
     .then((resp) => resp.json())
-    .then((data) => renderTrainers(data))
+    .then((trainers) => renderTrainers(trainers))
     .catch(error => console.log(error))
 }
 
-const fetchPokemons = () => {
-  fetch(POKEMONS_URL)
-    .then((resp) => resp.json())
-    .then((pokemons) => renderPokemon(pokemons))
-    .catch(error => console.log(error))
-};
-
-const renderTrainers = (data) => {
-  trainers = data
+const renderTrainers = (trainers) => {
   trainers.forEach(trainer => renderTrainer(trainer))
 }
 
-const renderTrainer = (trainer) => {
-  const newTrainerContainer = document.createElement('div')
-  const container = document.getElementById('poke-container')
+const renderTrainer = (trainer) =>{
+  let main = document.querySelector('main')
+  const trainerContainer = document.createElement('div')
+  const trainerName = document.createElement('h3')
   const addPokeBtn = document.createElement('button')
-  newTrainerContainer.classList.add('card')
-  addPokeBtn.innerHTML = 'Add Pokemon'
-  addPokeBtn.dataset.id = trainer.id
-  newTrainerContainer.dataset.id = trainer.id 
-  newTrainerContainer.innerHTML = trainer.name
-  container.appendChild(newTrainerContainer)
-  container.appendChild(addPokeBtn)
+
+  addPokeBtn.dataset.trainerId = trainer.id
+  trainerContainer.dataset.id = trainer.id
+  trainerName.innerHTML = trainer.name
+  addPokeBtn.innerText = 'Add Pokemon'
+  trainerContainer.classList.add("card");
+
+  trainerContainer.appendChild(addPokeBtn)
+  trainerContainer.appendChild(trainerName)
+  main.appendChild(trainerContainer)
+
+  renderPokemons(trainer.pokemons)
 }
 
+const renderPokemons = (pokemons) => {
+  pokemons.forEach(pokemon => renderPokemon(pokemon))
+}
 
+const renderPokemon = (pokemon) => {
+  const trainerContainer = document.querySelector(`[data-id="${pokemon.trainer_id}"]`);
+  const pokeDiv = document.createElement('ul')
+  const pokeName = document.createElement('li')
+  const pokeSpecies = document.createElement('li')
+  const removeBtn = document.createElement('button')
+  removeBtn.classList.add('release')
+  removeBtn.innerText = 'Release Me'
+  pokeName.innerHTML = `name: ${pokemon.nickname}`
+  pokeSpecies.innerHTML = `species: ${pokemon.species}`
+  pokeDiv.appendChild(removeBtn)
+  pokeDiv.appendChild(pokeName)
+  pokeDiv.appendChild(pokeSpecies)
+  trainerContainer.appendChild(pokeDiv)
 
-const renderPokemon = (pokemons) => {
-  Array.from(pokemons).filter(pokemon => 
-    {if (pokemon.trainer_id = trainers.id){
-    pokemons.slice(0, 5).forEach(pokemon => {
-      let trainerDiv = document.querySelector(`[data-id='${pokemon.trainer_id}']`);
-      const pokemonsUl = document.createElement("ul");
-    
-      console.log(pokemon.trainer_id)
-      const pokeLi = document.createElement('li')
-      const removePokeBtn = document.createElement("button");
-      pokeLi.innerHTML = pokemon.nickname
-      
-      removePokeBtn.innerHTML = "Remove Pokemon";
-      removePokeBtn.classList.add('release')
-      removePokeBtn.dataset.pokemonid = pokemon.id
-      pokemonsUl.appendChild(pokeLi)
-      pokemonsUl.appendChild(removePokeBtn)
-      trainerDiv.appendChild(pokemonsUl)
-    })}}}
-    
-    
-
-};
-
-
-
+}
 document.addEventListener('DOMContentLoaded', (e) => {
   fetchTrainers()
-  fetchPokemons()
+
 })
